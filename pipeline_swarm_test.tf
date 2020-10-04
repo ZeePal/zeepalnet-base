ac5593dde23dcba18830d3da48d57e829c88d4ce
ZeePal <pallsjh@gmail.com>
2020-10-04 21:55:43 +1100

    provide host to use

--- pipeline_swarm_test.tf
+++ pipeline_swarm_test.tf
@@ -9,4 +9,8 @@ module pipeline_swarm_test {
   repo_name  = "zeepalnet-gcp-swarm-test"
 
   override_terraform_docker_image = "gcr.io/$PROJECT_ID/terraform-with-gcloud-ssh"
+
+  extra_env_vars = [
+    "TF_VAR_host=swarm-1"
+  ]
 }
