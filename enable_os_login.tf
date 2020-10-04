e5a1f250193ea3a38906e18ac783d6459c3f0bf3
ZeePal <pallsjh@gmail.com>
2020-10-04 21:48:54 +1100

    too much/buggy 2fa

--- enable_os_login.tf
+++ enable_os_login.tf
@@ -2,8 +2,3 @@ resource google_compute_project_metadata_item oslogin {
   key   = "enable-oslogin"
   value = "TRUE"
 }
-
-resource google_compute_project_metadata_item oslogin_2fa {
-  key   = "enable-oslogin-2fa"
-  value = "TRUE"
-}
