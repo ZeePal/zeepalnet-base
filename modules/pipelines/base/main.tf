010c467f123bcb3b04c28c5fd5726ac09d61d31f
ZeePal <pallsjh@gmail.com>
2020-10-07 00:41:31 +1100

    Revert "trying kaniko"
    This reverts commit a561dfe89a1ac70ffdee418b7e81e5a8676be01d.

--- modules/pipelines/base/main.tf
+++ modules/pipelines/base/main.tf
@@ -23,7 +23,8 @@ resource google_cloudbuild_trigger pipeline {
   build {
     artifacts {
       images = [
-        "${local.docker_image_prefix}/git-verify:latest"
+        "${local.docker_image_prefix}/git-verify:latest",
+        "${local.docker_image_prefix}/terraform-with-gcloud-ssh:latest"
       ]
     }
 
@@ -66,13 +67,23 @@ EOS
     }
 
     step {
-      id   = "build gcloud ssh wrapper image"
-      name = "gcr.io/kaniko-project/executor"
+      id       = "pull gcloud image for caching"
+      wait_for = ["git verify"] # TF & Docker Builds can run in parallel
+      name     = "gcr.io/cloud-builders/docker"
       args = [
-        "--context", "docker/terraform-with-gcloud-ssh/",
-        "--destination", "${local.docker_image_prefix}/terraform-with-gcloud-ssh:latest",
-        "--cache=true",
-        "--build-arg", "terraform_version=${var.terraform_version}"
+        "pull", "gcr.io/cloud-builders/gcloud"
+      ]
+    }
+    step {
+      id       = "build gcloud ssh wrapper image"
+      wait_for = ["pull gcloud image for caching"]
+      name     = "gcr.io/cloud-builders/docker"
+      args = [
+        "build",
+        "-t", "${local.docker_image_prefix}/terraform-with-gcloud-ssh:latest",
+        "--build-arg", "terraform_version=${var.terraform_version}",
+        "--cache-from", "${local.docker_image_prefix}/terraform-with-gcloud-ssh:latest",
+        "docker/terraform-with-gcloud-ssh/"
       ]
     }
 
