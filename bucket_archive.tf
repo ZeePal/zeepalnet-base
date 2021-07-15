5c0cc2e10893efc8dd73ad2102453f8d7778b386
ZeePal <pallsjh@gmail.com>
2021-07-15 14:45:42 +1000

    extra archive bucket failsafe

--- bucket_archive.tf
+++ bucket_archive.tf
@@ -17,4 +17,10 @@ resource "google_storage_bucket" "archive" {
       age = 1
     }
   }
+
+  # Extra soft failsafe against deletions WITHOUT locking the policy to the bucket...
+  retention_policy {
+    # "The value must be less than 2,147,483,647 seconds."
+    retention_period = 2147483646
+  }
 }
