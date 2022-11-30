981afe5b79960e5b23f0a5de9f52eeba2e36baef
Paul Harrington <pallsjh@gmail.com>
2022-11-30 21:09:10 +1100

    updated gcp provider

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.3.5"
   required_providers {
-    google = { version = "~> 3.41" }
+    google = { version = "~> 4.44" }
 
     # Beta provider used for:
     # * Cloud Build Trigger with GitHub functionality
-    google-beta = { version = "~> 3.41" }
+    google-beta = { version = "~> 4.44" }
   }
 }
